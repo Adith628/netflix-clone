dd8d30b2b914940c940f59a04b715f0ef53f706b
Adithyan T <adithsreedhar628@gmail.com>
2023-03-13 23:45:35 +0530

    different genre of movies are fetched using api

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import './Row.css'
+import React, { useEffect, useState } from 'react'
+import './Row.css';
+import axios from '../../axios';
 
-function Row({title}) {
+function Row({title,fetchURL,isLargeRow }) {
+
+    const [movies,setMovies] = useState([]);
+    const  base_url = "https://image.tmdb.org/t/p/original/";
+
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(fetchURL);
+            setMovies(request.data.results);
+            return request;
+        }
+
+        fetchData();
+
+    },[fetchURL]);
+
+    console.log(movies);
+
+     
   return (
     <div className='row'>
       <h2>{title}</h2>
+      {movies.map(movie =>(
+        <img 
+        className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+        src={`${base_url}${
+            isLargeRow ? movie.poster_path : movie.backdrop_path
+        }`} alt="movie.name" />
+      ))}
     </div>
   )
 }
