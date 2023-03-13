45fb5859aeb3565934dd508d41f6e60773252486
Adithyan T <adithsreedhar628@gmail.com>
2023-03-13 23:29:29 +0530

    updated

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAqlpl_Aj2AKoO8CdCK_ehOXEv8GAFTDv4",
+  authDomain: "netflix-clone-1f25d.firebaseapp.com",
+  projectId: "netflix-clone-1f25d",
+  storageBucket: "netflix-clone-1f25d.appspot.com",
+  messagingSenderId: "149505000967",
+  appId: "1:149505000967:web:ee475f3ba62c6d08084ca2"
+};
---
+++ src/components/NavBar/Nav.js
@@ -0,0 +1,45 @@
+import React,{useEffect,useState} from 'react'
+import "./Nav.css"
+import logo from "../../images/logo.png";
+
+function Nav() {
+
+  const [show,handleShow] = useState(false);
+
+  const transitionNavbar =()=>{
+    if(window.scrollY>100){
+      handleShow(true);
+    }
+    else{
+      handleShow(false);
+    }
+  }
+
+  useEffect(()=>{
+    window.addEventListener("scroll",transitionNavbar);
+    return()=>window.removeEventListener("scroll",transitionNavbar);
+  },[])
+
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <div className="nav_contents">
+        <img
+          className='nav_logo'
+          src={logo} 
+          alt="logo"
+        />
+ 
+        <img 
+          className='nav_avatar'
+          src="https://i.pinimg.com/originals/31/2e/8e/312e8ec9f9581d8f606bfa875b302bcb.jpg"
+          alt="avatar"
+        />
+      </div>
+      
+    </div>
+  )
+}
+ 
+
+export default Nav
