85a0cee07b9069dd701125321c7934142aa94895
Adithyan T <adithsreedhar628@gmail.com>
2023-03-15 01:50:26 +0530

    firebase auth is setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyAqlpl_Aj2AKoO8CdCK_ehOXEv8GAFTDv4",
   authDomain: "netflix-clone-1f25d.firebaseapp.com",
@@ -6,3 +11,12 @@ const firebaseConfig = {
   messagingSenderId: "149505000967",
   appId: "1:149505000967:web:ee475f3ba62c6d08084ca2"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+
+
+export {auth};
+export default db;
